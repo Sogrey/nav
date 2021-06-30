694cd16d6b6dbe760a98b933a5844e47d9fa031d
sogrey <suzhaoheng2006@163.com>
2021-06-30 23:43:21 +0800

    init

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/Sogrey/nav',
+  gitRepoUrl: 'https://github.com/Sogrey/nav.git',
 
   // 部署分支
   branch: 'main',
